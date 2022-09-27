819d71ee723b4ba8731d4c06940f98126d90d524
Harvey Unsaac <193837@unsaac.edu.pe>
2022-09-27 17:54:45 +0200

    :sparkles: Add uses of strings like template literals and multiline string

---
+++ src/es6/03-strings.js
@@ -0,0 +1,18 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase);
+
+// Template literals
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// Multiline strings
+let lorem = 'esto es una linea \n' + 'esto es otra linea';
+
+let lorem2 = `Esta es una frase epica
+esta es la continuacion de la frase`;
+
+console.log(lorem);
+console.log(lorem2);
+
