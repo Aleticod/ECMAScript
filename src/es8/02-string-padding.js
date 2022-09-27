945642fa87728c7f3541c335f2c4efc0c8e7e376
Harvey Unsaac <193837@unsaac.edu.pe>
2022-09-28 01:16:45 +0200

    :sparkles: Add string padding and trailing ocmmas in ES8

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,3 @@
+const word = 'hello';
+console.log(word.padStart(8, '_').padEnd(11,'_'))
+console.log(word.padEnd(8,'_'))
---
+++ src/es8/03-trailing-commas.js
@@ -0,0 +1,4 @@
+const ages = [13, 53, 63, 64, , , , 45];
+
+console.log(ages);
+console.log(ages.length);
