8322e3b110d2bbd22165d15dd8f9ccdc83d0ffac
Harvey Unsaac <193837@unsaac.edu.pe>
2022-09-27 18:10:36 +0200

    :sparkles: Add array and object destructuring in ES6

---
+++ src/es6/05-rest-spread.js
@@ -0,0 +1,10 @@
+
+// Arrays destructuring
+let fruits = ['Apple', 'Banana'];
+let [a, b] = fruits;
+console.log(a , b);
+
+// Object destructuring
+let user = {username: 'Oscar', age: 43};
+let {username, age} = user;
+console.log(username, age);
