dffaaba1c5c03415f5fc06d20144def88e6f463a
Harvey Unsaac <193837@unsaac.edu.pe>
2022-09-27 17:42:46 +0200

    :sparkles: Add arrow functions applications modified

---
+++ src/es6/001-arrow-function.js
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * @param {*} num 
+ * @returns the number square
+ */
+function square (num) {
+    return num * num;
+}
+
+/**
+ * Arrow Function
+ */
+const square = (num) => {
+    return num * num;
+}
+
+const square = num => num * num;
