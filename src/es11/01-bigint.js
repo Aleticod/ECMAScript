ee4347010d044faefbe63c0b6d4cc921edd5d359
Harvey Unsaac <193837@unsaac.edu.pe>
2022-09-28 06:50:29 +0200

    :sparkles: Add gitint and nullish operator in ES11

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 34543534534543534534535n;
+const bBigNumber = BigInt(34543534534543534534535);
+console.log(aBigNumber);
+console.log(bBigNumber);
---
+++ src/es11/02-nullish.js
@@ -0,0 +1,3 @@
+const number = undefined; // this would be null or undefined
+const validate = number ?? 5;
+console.log(validate);
